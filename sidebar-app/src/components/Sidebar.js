853c9b5296b243c27bd88639d65a7b8cd60bbef4
shimakimi <imperialbloodrespect@gmail.com>
2024-02-22 22:09:00 +0900

    logo テスト削除

---
+++ sidebar-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      {/* Rest of your app */}
+    </div>
+  );
+}
+
+export default App;
